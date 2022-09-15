2e6cc5329d529bf6ffd0b994adebe57f4f7c1968
Nganga <nganga.inc@gmail.com>
2022-09-15 10:06:32 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - @ntry point
+ * description - Write a function that prints the alphabet, in lowercase,
+ * followed by a new line
+ * return 0 (Always)
+ * main(void) - parameters of function main that return the value 0
+ * when main is called
+ */
+
+int main(void)
+{
+	void print_alphabet(void);
+	{
+		int x;
+		for (x = 'a' ; x = 'z' ; x++)
+			putchar(x);
+		return print_alphabet;
+	}
+	return(0);
+}
