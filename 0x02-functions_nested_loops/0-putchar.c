e2a4f2c40a65919983fbd8e0e2bc86fd38e19a09
Nganga <nganga.inc@gmail.com>
2022-09-15 07:44:26 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description : program that prints _putchar, followed by a new line.
+ * return 0(Always)
+ */
+
+int main(void);
+{
+	printf("_putchar");
+
+	return (0);
+}
